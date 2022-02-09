fd9f0798330019d8b109201412caacad5f7a66fc
Johannes Nilsson <johannes@nilsson.im>
2022-02-09 15:41:52 +0100

    Small fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addWatchTarget('./src/assets/styles/');
-    eleventyConfig.addPassthroughCopy('./src/assets/images/');
-    eleventyConfig.addPassthroughCopy('./src/assets/scripts/');
+  eleventyConfig.addWatchTarget("./src/assets/styles/");
+  eleventyConfig.addPassthroughCopy("./src/assets/images/");
+  eleventyConfig.addPassthroughCopy("./src/assets/scripts/");
 
-    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-  
-    return {
-      dir: {
-        input: 'src',
-        output: 'public',
-      },
-    };
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
   };
+};
